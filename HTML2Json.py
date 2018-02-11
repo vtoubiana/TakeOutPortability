946f454bb2a594d7203e0581a55cbf7109006a01
Janos <v.toubiana@free.fr>
2018-02-12 00:08:38 +0100

    add meta data when availabl

--- HTML2Json.py
+++ HTML2Json.py
@@ -4,27 +4,37 @@ from BeautifulSoup import BeautifulSoup
 from bs4 import BeautifulSoup
 import json
 
-array = [] 
+DIV_SELECTOR = "outer-cell mdl-cell mdl-cell--12-col mdl-shadow--2dp"
+EVENT_SELECTOR = "content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1"
+META_SELECTOR = "content-cell mdl-cell mdl-cell--12-col mdl-typography--caption"
+
+
 
 def isVisit(text):
 	return text.find('http') == 0
-
+# content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1
+# content-cell mdl-cell mdl-cell--12-col mdl-typography--caption
 
 def parseHTML(page, output_f):
+	array = [] 
 	soup = BeautifulSoup(page,'html.parser')
-	for div in soup.find_all('div',class_ ="content-cell mdl-cell mdl-cell--6-col mdl-typography--body-1"):
+	for div in soup.find_all('div',class_ = DIV_SELECTOR):
 		try:
-			elt ={}
-			link = div.find('a')
-			text = link.get_text()
-			if isVisit(text):
-				elt['visit'] = text
-			else :
-				elt['query_text'] = text
-			date = div.find('br').get_text()
-			elt['link'] = link["href"]
-			elt['date'] = date
-			array.append(elt)
+			event = div.find('div',class_ = EVENT_SELECTOR)
+			if event:
+				elt ={}
+				link = event.find('a')
+				text = link.get_text()
+				if isVisit(text):
+					elt['visit'] = text
+				else :
+					elt['query_text'] = text
+				elt['link'] = link["href"]
+				elt['date'] = event.find('br').get_text()
+				meta = div.find('div',class_ =META_SELECTOR)
+				if meta :
+					elt['meta'] = meta.get_text()
+				array.append(elt)
 		except:
 			print "Failed to parse elt"	
 
